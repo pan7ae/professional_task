e81ace0a50d1c81bd0167d30262e2bae83e4c2b7
pan7ae <tolikpan0403@gmail.com>
2023-06-08 13:46:25 +0700

    Fixed 10th issue (SRP)
    Класс PromoRepository отвечает только за операции с базой данных, а класс PromoService предоставляет бизнес-логику и обработку HTTP-запросов, используя функциональность репозитория

--- src/promotion/services/promo.py
+++ src/promotion/services/promo.py
@@ -7,11 +7,11 @@ from ..db.promotion import promotions
 from ..models.models import Promo, PromoWithDetails, PromoCreate, PromoUpdate
 
 
-class PromoService:
+class PromoRepository:
     def __init__(self, database: Database):
         self.database = database
 
-    async def get_promo_with_details(self, promo_id: int) -> PromoWithDetails:
+    async def get_promo_by_id(self, promo_id: int) -> PromoWithDetails:
         query = promotions.select().where(promotions.c.id == promo_id)
         promo = await self.database.fetch_one(query=query)
 
@@ -24,7 +24,7 @@ class PromoService:
         return await self.database.fetch_all(query=query)
 
     async def get_promo(self, promo_id: int) -> PromoWithDetails:
-        return await self.get_promo_with_details(promo_id)
+        return await self.get_promo_by_id(promo_id)
 
     async def create_promo(self, promo_data: PromoCreate):
         promo = PromoCreate(**promo_data.dict())
@@ -35,7 +35,7 @@ class PromoService:
         return promo_res
 
     async def update_promo(self, promo_id: int, promo_data: PromoUpdate) -> Promo:
-        promo = await self.get_promo_with_details(promo_id)
+        promo = await self.get_promo_by_id(promo_id)
 
         for field, value in promo_data:
             if value:
@@ -47,8 +47,32 @@ class PromoService:
         return promo
 
     async def delete_promo(self, promo_id: int) -> None:
-        await self.get_promo_with_details(promo_id)
+        await self.get_promo_by_id(promo_id)
 
         query = promotions.delete().where(promotions.c.id == promo_id)
         return await self.database.execute(query=query)
+
+
+
+class PromoService:
+    def __init__(self, promo_repository: PromoRepository):
+        self.promo_repository = promo_repository
+
+    async def get_promo_with_details(self, promo_id: int) -> PromoWithDetails:
+        return await self.promo_repository.get_promo_by_id(promo_id)
+
+    async def get_promo_list(self) -> List[Promo]:
+        return await self.promo_repository.get_promo_list()
+
+    async def get_promo(self, promo_id: int) -> PromoWithDetails:
+        return await self.promo_repository.get_promo(promo_id)
+
+    async def create_promo(self, promo_data: PromoCreate):
+        return await self.promo_repository.create_promo(promo_data)
+
+    async def update_promo(self, promo_id: int, promo_data: PromoUpdate) -> Promo:
+        return await self.promo_repository.update_promo(promo_id, promo_data)
+
+    async def delete_promo(self, promo_id: int) -> None:
+        return await self.promo_repository.delete_promo(promo_id)
 
