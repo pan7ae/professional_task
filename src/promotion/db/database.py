b04559e84cb56b1b38d83601bb8428a8fdaebd08
pan7ae <tolikpan0403@gmail.com>
2023-06-08 12:48:49 +0700

    Fixed 8th issue (Timeout error)
    Также добавлены параметры min_size & max_size, определяющий минимальное и максимально кол-во подключений в пуле

--- src/promotion/db/database.py
+++ src/promotion/db/database.py
@@ -2,7 +2,7 @@ from databases import Database
 from sqlalchemy import create_engine, MetaData
 from ..config import DATABASE_URL
 
-database = Database(DATABASE_URL)
+database = Database(DATABASE_URL, min_size=1, max_size=20, timeout=60)
 metadata = MetaData()
 engine = create_engine(
     DATABASE_URL,
